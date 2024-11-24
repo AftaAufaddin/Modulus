538fed50b48896251611f1d22879f6af5f9a6b08
AftaAufaddin <aftaaufaddin@gmail.com>
2024-11-24 09:45:08 +0700

    MatematikaCanggihBeraksi.java

--- MatematikaCanggihBeraksi.java
+++ MatematikaCanggihBeraksi.java
@@ -20,7 +20,7 @@ public class MatematikaCanggihBeraksi {
         System.out.print("Masukkan Nilai 2 : ");
         int nilai2 = input.nextInt();
         
-        System.out.println("\nHasil Pertambahan " + nilai1 + " + " +nilai2 + " = " + mtk.pertambahan(nilai1, nilai2));
+        System.out.println("Hasil Pertambahan " + nilai1 + " + " +nilai2 + " = " + mtk.pertambahan(nilai1, nilai2));
         System.out.println("Hasil Perkalian " + nilai1 + " * " +nilai2 + " = " + mtk.perkalian(nilai1, nilai2));
         System.out.println("Hasil Modulus " + nilai1 + " % " +nilai2 + " = " + mtk.modulus(nilai1, nilai2));
     }
